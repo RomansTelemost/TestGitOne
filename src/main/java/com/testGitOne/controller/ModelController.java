e02b64550786e4a615d1abf09b88aa175fdf0d2e
ru <epsis@mail.ru>
2024-01-03 22:17:11 +0300

    Git stash

--- src/main/java/com/testGitOne/controller/ModelController.java
+++ src/main/java/com/testGitOne/controller/ModelController.java
@@ -4,9 +4,6 @@ public class ModelController {
     public ModelController() {
         System.out.println("features1");
         int a;
-        int b;
-        int c;
         int d;
-        int i;
     }
 }
